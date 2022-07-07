52faa5944c6a4c0b771737555255b2d26f17a355
Semen <semen.manushko@gmail.com>
2022-07-07 11:39:26 +0200

    added multiselect

--- Hello.py
+++ Hello.py
@@ -11,9 +11,14 @@ def main():
     with st.sidebar:
         add_country = st.selectbox("Choose a Country", ('Russia', 'Australia', 'Argentina', 'Canada'))
         df = download_dataframe(creds=creds, filename=f'cash_prices_{add_country.lower()}.csv', parse_dates=['TRADEDATE'])
+        min_start_wwht, max_start_wwht = df['TRADEDATE'].min(), df['TRADEDATE'].max()
         add_category = st.multiselect("Choose a Category", list(df['NAME'].unique()))
+        
+        start = st.date_input("Start Date", min_start_wwht, min_value=min_start_wwht, max_value=max_start_wwht)
+        end = st.date_input("End Date", max_start_wwht, min_value=min_start_wwht, max_value=max_start_wwht)
+        
     for cat in add_category:
-        st.plotly_chart(get_chart(df.query('NAME==@cat'), 'TRADEDATE', 'CLOSE', f'{add_country} - Cash Prices'))
+        st.plotly_chart(get_chart(df.query('NAME==@cat & TRADEDATE>=@start & TRADEDATE<=@end'), 'TRADEDATE', 'CLOSE', f'{add_country} - {cat} Cash Prices'))
 
     
 if __name__ == '__main__':
