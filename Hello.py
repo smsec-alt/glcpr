b0c0df2d2ed0911228f0825c2f8c35d70f123218
Semen <semen.manushko@gmail.com>
2022-07-07 11:40:38 +0200

    adding calendar

--- Hello.py
+++ Hello.py
@@ -13,9 +13,9 @@ def main():
         df = download_dataframe(creds=creds, filename=f'cash_prices_{add_country.lower()}.csv', parse_dates=['TRADEDATE'])
         min_start_wwht, max_start_wwht = df['TRADEDATE'].min(), df['TRADEDATE'].max()
         add_category = st.multiselect("Choose a Category", list(df['NAME'].unique()))
-        
-        start = st.date_input("Start Date", min_start_wwht, min_value=min_start_wwht, max_value=max_start_wwht)
-        end = st.date_input("End Date", max_start_wwht, min_value=min_start_wwht, max_value=max_start_wwht)
+        col1, col2 = st.columns(2)
+        start = col1.date_input("Start Date", min_start_wwht, min_value=min_start_wwht, max_value=max_start_wwht)
+        end = col2.date_input("End Date", max_start_wwht, min_value=min_start_wwht, max_value=max_start_wwht)
         
     for cat in add_category:
         st.plotly_chart(get_chart(df.query('NAME==@cat & TRADEDATE>=@start & TRADEDATE<=@end'), 'TRADEDATE', 'CLOSE', f'{add_country} - {cat} Cash Prices'))
