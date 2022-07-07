c3093538fd81822ad65fa2b8d0d15c696ec33ffa
Semen <semen.manushko@gmail.com>
2022-07-07 10:30:14 +0200

    added logging

--- Hello.py
+++ Hello.py
@@ -1,10 +1,8 @@
-import logging
 import streamlit as st
 from quickstart import credentials, download_dataframe
 from resources import get_chart
 
 
-logging.getLogger('googleapicliet.discovery_cache').setLevel(logging.ERROR)
 st.set_page_config(page_title="Cash Prices", layout='wide',)
 creds = credentials()
 
@@ -13,9 +11,11 @@ with st.sidebar:
 
 
 def main():   
-    df = download_dataframe(creds=creds, filename=f'cash_prices_{add_country}.csv', parse_dates=['TRADEDATE'])
-    st.write(df)
-    st.plotly_chart(get_chart(df, 'TRADEDATE', 'CLOSE', f'{add_country} - Cash Prices'))
+    add_country = 'Russia'
+    df = download_dataframe(creds=creds, filename=f'cash_prices_{add_country.lower()}.csv', parse_dates=['TRADEDATE'])
+    # print(df)
+    # st.write(df)
+    # st.plotly_chart(get_chart(df, 'TRADEDATE', 'CLOSE', f'{add_country} - Cash Prices'))
     # st.write('123')
     
 if __name__ == '__main__':
