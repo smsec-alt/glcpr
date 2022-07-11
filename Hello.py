e1b2c0e47d193ae3693d7ac2d467ee58aad93761
Semen <semen.manushko@gmail.com>
2022-07-11 15:11:34 +0200

    added spread charts

--- Hello.py
+++ Hello.py
@@ -19,7 +19,7 @@ def main():
             if add_2legs:
                 col11, col21 = st.columns(2)
                 leg1 = col11.selectbox("Leg 1", all_categories)
-                leg2 = col21.selectbox("Leg 2", all_categories)
+                leg2 = col21.selectbox("Leg 2", all_categories[::-1])
                 add_operation = st.selectbox("Select a function", ('Spread', 'Ratio'))
             else:
                 add_category = st.selectbox("Choose a Category", all_categories)
@@ -34,7 +34,23 @@ def main():
                         
     # for cat in add_category:
     #     st.plotly_chart(get_chart(df.query('NAME==@cat & TRADEDATE>=@start & TRADEDATE<=@end'), 'TRADEDATE', 'CLOSE', f'{add_country} - {cat} Cash Prices', logs=add_logs))
-    st.plotly_chart(get_chart(df.query('NAME==@add_category & TRADEDATE>=@start & TRADEDATE<=@end'), 'TRADEDATE', 'CLOSE', f'{add_country} - {add_category} Cash Prices', logs=add_logs))
+    if len(all_categories) > 1:
+        subdf = df.query('TRADEDATE>=@start & TRADEDATE<=@end')
+        # st.plotly_chart(get_chart(subdf, 'TRADEDATE', 'CLOSE', f'{add_country} - {add_category} Cash Prices', logs=add_logs))
+        
+        df_cat1, df_cat2 = subdf.query('NAME == @leg1'), subdf.query('NAME == @leg2')
+        df_cat1.rename({'CLOSE': leg1}, axis=1, inplace=True)
+        df_cat2.rename({'CLOSE': leg2}, axis=1, inplace=True)
+        df_all = df_cat1.merge(df_cat2, on='TRADEDATE')
+        if add_operation == 'Spread':
+            df_all['Result'] = df_all[leg1] - df_all[leg2]
+        else:
+            df_all['Result'] = df_all[leg1] / df_all[leg2]
+        st.plotly_chart(get_chart(df_all, 'TRADEDATE', 'Result', f'{add_country} - {leg1}-{leg2} {add_category} Cash Prices'))
+        
+    else:    
+        subdf = df.query('NAME==@add_category & TRADEDATE>=@start & TRADEDATE<=@end')
+        st.plotly_chart(get_chart(subdf, 'TRADEDATE', 'CLOSE', f'{add_country} - {add_category} Cash Prices', logs=add_logs))
 
 # np.log2(data['Salary'])
     
